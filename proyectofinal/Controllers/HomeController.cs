5198974d767306ea6ef011a51795ffc38bd74e61
Jerson_web <gersreyes01@gmail.com>
2022-02-05 16:48:36 -0600

    cambio en controlador modelo e index se creo nueva clase

--- proyectofinal/Controllers/HomeController.cs
+++ proyectofinal/Controllers/HomeController.cs
@@ -24,9 +24,27 @@ namespace proyectofinal.Controllers
             //simulador de base de datos 
                 
             List<prestamos> prestamos = new List<prestamos>();
-            
+            prestamos.Add(new prestamos()
+            {
+                Nombre = "Jerson",
+                Apellido = "Reyes",
+                Cedula = "0012206951206J",
+                Direccion = "direccion cualquiera",
+                Telefono = "12345678",
+                Fecha_de_Prestamo = "12/01/22",
+                Monto = "25,000",
+                Interes="15%",
+                Plazo = "6 meses",
+                Edad = 25,
+                
+            });
 
             return View(prestamos);
+        }
+
+        public IActionResult ingresodatos()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
--- proyectofinal/Models/prestamos.cs
+++ proyectofinal/Models/prestamos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,46 @@ namespace proyectofinal.Models
 {
     public class prestamos
     {
+        [Key]
+        public int Id { get; set; }
+       
+        [StringLength(100)]
+        [Required(ErrorMessage ="Este campo es requerido")]
         public string Nombre { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Apellido { get; set; }
+        
+        [StringLength(100)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Cedula { get; set; }
+        
+        [StringLength(500)]
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string Direccion { get; set; }
+        
+        [StringLength(30)]
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string Telefono { get; set; }
 
+        [StringLength(30)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Fecha_de_Prestamo { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Monto { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Interes { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Plazo { get; set; }
+
+        [StringLength(20)]
         public int Edad { get; set; }
     }
 }
